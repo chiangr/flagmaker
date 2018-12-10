4bc73c62b61412332a9e6bec26dc7eaf0613b080
rchiang <chiang.ryan@hotmail.com>
2018-12-10 09:38:54 -0800

    Initialize Framework

---
+++ marker.py
@@ -0,0 +1,11 @@
+from Tkinter import *
+import PIL
+from PIL import ImageTk
+import Tkinter as Tk
+
+import os.path
+root = Tk()
+
+canvas = Canvas(root, height = 600, width = 1000, bg='red')
+
+root.mainloop()
